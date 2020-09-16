89ee008432210c744a344bccd5100ff752ef1d7e
tlkj_zhx <924434073@qq.com>
2020-09-16 18:48:55 +0800

    55

--- promise/pro.js
+++ promise/pro.js
@@ -8,24 +8,24 @@ function readFileByPath(path) {
     })
     return p;
 }
-// readFileByPath("./test1.txt").then(res => {
-//     console.log(res)
-//     return readFileByPath("./test2.txt")
-// }).catch(err => {
-//     console.log("err",err)
-//     return readFileByPath("./test3.txt")
-// }).then(res => {
-//     console.log("res")
-//     console.log(res)
-// })
-async function test() {
-    let r1 = await readFileByPath("./test1.txt");
-    console.log(r1)
-    let r2 = await readFileByPath("./test2.txt");
-    console.log(r2)
-    let r3 = await readFileByPath("./test3.txt");
-    console.log(r3)
-    let r4 = await readFileByPath("./test4.txt");
-    console.log(r4)
-}
-test()
+readFileByPath("./test1.txt").then(res => {
+    console.log(res)
+    // return readFileByPath("./test2.txt")
+}).catch(err => {
+    console.log("err",err)
+    return readFileByPath("./test3.txt")
+}).then(res => {
+    console.log("res")
+    console.log(res)
+})
+// async function test() {
+//     let r1 = await readFileByPath("./test1.txt");
+//     console.log(r1)
+//     let r2 = await readFileByPath("./test2.txt");
+//     console.log(r2)
+//     let r3 = await readFileByPath("./test3.txt");
+//     console.log(r3)
+//     let r4 = await readFileByPath("./test4.txt");
+//     console.log(r4)
+// }
+// test()
